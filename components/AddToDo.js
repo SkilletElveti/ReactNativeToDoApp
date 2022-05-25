b34ec09cea3b8263f36180b2feaf184ea7491516
shubhamkamdi <shubhamvkamdi@gmail.com>
2022-05-25 15:45:29 -0700

    - Updated AddToDo.js

--- components/AddToDo.js
+++ components/AddToDo.js
@@ -2,11 +2,27 @@ import { StyleSheet, Text, TextInput, Button } from 'react-native';
 import React, { useState } from 'react';
 
 export default function AddToDoForm() {
+    const [text, setText] = useState('')
+    const changeHandler = (val) => {
+        setText(val);
+    }
     return (
-        <></>
+        <View>
+            <TextInput
+                style={styles.input}
+                placeholder='New ToDo.....'
+                onChange={{changeHandler}}
+            />
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
-    
+    input: {
+        marginBottom: 10,
+        paddingHorizontal: 8,
+        paddingVertical: 6,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ddd'
+    },
 })
