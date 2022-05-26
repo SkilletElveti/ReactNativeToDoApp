c6771040d20493ab3bf22b0bc935a2af65320bba
shubhamkamdi <shubhamvkamdi@gmail.com>
2022-05-26 14:57:33 -0700

    - Changes

--- components/Todo.js
+++ components/Todo.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons'
+export default function ToDo({ item, handler }) {
 
-export default function ToDo({item,handler}) {
-    
-    return(
-        <TouchableOpacity onPress={() => {handler(item.key)}}>
-            <Text style={styles.item}>{item.text}</Text>
+    return (
+        <TouchableOpacity onPress={() => { handler(item.key) }} style={styles.item}>
+            <Text style={styles.text}>{item.text}</Text>
+            <MaterialIcons name='delete' style={styles.icon} size={18} color='#333'/>
         </TouchableOpacity>
     )
 }
@@ -13,10 +14,19 @@ export default function ToDo({item,handler}) {
 const styles = StyleSheet.create({
     item: {
         padding: 16,
+        flexDirection: 'row',
+        alignItems:'center',
         marginTop: 16,
         borderColor: '#bbb',
         borderWidth: 1,
         borderStyle: 'dashed',
         borderRadius: 10,
+    },
+    text:{
+        flex:1,
+    },
+    icon: {
+        alignItems:'flex-end',
+        padding:5,
     }
 })
